b6083e14483d851870376d381e57edb84a459a21
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-31 20:58:49 -0300

    Different styles for normal and maximized window
    Don't use lines/borders on maximized window

--- framlr.js
+++ framlr.js
@@ -3,6 +3,18 @@
             const {BrowserWindow} = require('electron').remote
 
             function init() {
+            
+            var window = BrowserWindow.getFocusedWindow();
+            if(window.isMaximized()){
+		document.getElementById("wm-window").classList.remove("framed");
+		document.getElementById("wm-window-title").classList.remove("framed");
+            }else{
+		document.getElementById("wm-window").classList.add("framed");
+		document.getElementById("wm-window-title").classList.add("framed");
+		//var element = document.getElementById('wm-window');
+                //element.classList.add('framed');
+            }
+            
                 // Minimize task
                 document.getElementById("minimize").addEventListener("click", (e) => {
                     var window = BrowserWindow.getFocusedWindow();
@@ -14,10 +26,14 @@
                     var window = BrowserWindow.getFocusedWindow();
                     if(window.isMaximized()){
                         window.unmaximize();
-						document.getElementById("maximize").classList.remove("restore");
+			document.getElementById("maximize").classList.remove("restore");
+			document.getElementById("wm-window").classList.add("framed");
+		        document.getElementById("wm-window-title").classList.add("framed");
                     }else{
                         window.maximize();
-						document.getElementById("maximize").classList.add("restore");
+			document.getElementById("maximize").classList.add("restore");
+			document.getElementById("wm-window").classList.remove("framed");
+		        document.getElementById("wm-window-title").classList.remove("framed");
                     }
                 });
 
